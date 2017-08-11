7cfedbf7887dd332be3646978765db4e2d996342
Beny-Muler <beny@qanta.ai>
2017-08-11 15:29:40 +0300

    toolbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdToolbarModule } from '@angular/material';
+import {FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    //Angular material imports
+    MdButtonModule,
+    MdCheckboxModule,
+    MdToolbarModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
