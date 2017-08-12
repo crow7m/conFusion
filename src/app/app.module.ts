7ef4c95a972116d2544b94953a872753c936da73
Beny-Muler <beny@qanta.ai>
2017-08-12 22:24:42 +0300

    menu added selected added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import {MdButtonModule, MdCheckboxModule, MdToolbarModule, MdListModule, MdGridL
 import {FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailsComponent } from './dish-details/dish-details.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailsComponent
   ],
   imports: [
     BrowserModule,
